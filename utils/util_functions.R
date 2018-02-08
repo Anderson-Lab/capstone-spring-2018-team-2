13e49dbd1bc06bdb1ad8411815485cb45375be82
Alex Giarrocco <grocko95@gmail.com>
2018-02-08 11:38:02 -0500

    Closes #15 #12 - Added meta.rda file that maps meps codes to full descriptions

---
+++ utils/util_functions.R
@@ -0,0 +1,44 @@
+meps_scrape = function(){
+  library('rvest')
+  
+  load("data.rda")
+  
+  #Specifying the url for desired website to be scrapped
+  url = "utils/files/meps_codes.html"
+  
+  #Reading the HTML code from the website
+  webpage = read_html(url)
+  
+  #Using CSS selectors to scrap the rankings section
+  table_data_html = html_nodes(webpage,'.codes')
+  
+  #Converting the table data to text
+  table_data = html_text(table_data_html)
+  
+  # convert html table to a dataframe
+  codes_df = html_table(table_data_html, header = F, trim = T)[[1]]
+  
+  rownames(codes_df) = codes_df$X1
+  
+  codes_df$X1 = NULL
+  
+  # map the variable description in column 'X4' to the codes in rd.p
+
+  descriptions = sapply(colnames(rd.p), function(x) codes_df[x, 'X4'] )
+
+  save(descriptions, file = "meta.rda")
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
