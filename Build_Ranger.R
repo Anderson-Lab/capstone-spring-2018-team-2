898985ef9489d5936d2f8c4306a56d6fb7060d4a
sooonia <kopels@g.cofc.edu>
2018-02-12 11:31:56 -0500

    Switched var imp plot

--- Build_Ranger.R
+++ Build_Ranger.R
@@ -1,3 +1,4 @@
+
 library(ranger)
 library(caret)
 load('data.rda')
@@ -8,7 +9,6 @@ plan.dsn <- c('HOSPINSX', 'ANNDEDCT', 'HSAACCT', 'PLANMETL')
 behaviors <- c('BPCHEK53', 'CHOLCK53', 'NOFAT53', 'CHECK53', 'ASPRIN53', 'PAPSMR53', 'BRSTEX53', 'MAMOGR53', 'CLNTST53')
 controls <- c('CHBMIX42','BMINDX53','ADGENH42', 'AGE15X', 'FAMINC15', 'COBRA', 'OOPPREM', 'PREGNT31', 'PREGNT42', 'PREGNT53')
 target <- 'IPDIS15'
-descriptions[target]
 
 #split
 trainidx <- createDataPartition(rd.p$ERTOT15, p=.8, list = FALSE)
@@ -26,8 +26,16 @@ fit <- ranger(formula = f,
               sample.fraction = .8)
 
 preds <- as.data.frame(predict(fit, x.test)$predictions)
-varImpPlot(fit)
 imp <- fit$variable.importance
-save(imp, file = "r-shiny/template/data/ranger_imp.rda") # save the data to the r-shiny directory so that var.importance interactivity can be added
+#save(imp, file = "r-shiny/template/data/ranger_imp.rda") # save the data to the r-shiny directory so that var.importance interactivity can be added
 imp <- imp[imp > 50]
-barplot(imp, horiz=TRUE, las= 1)
+library(data.table)
+imp.dt<-setDT(as.data.frame(imp), keep.rownames = TRUE)[]
+library(ggplot)
+ggplot(data=imp.dt, aes(x=rn, y=imp)) +
+  geom_bar(stat="identity", fill = "dodgerblue3", color="black") + 
+  ggtitle('Variable Importance: Gini Impurity') +
+  xlab('Variables') +
+  ylab('Relative Importance')+
+  coord_flip()
+
