c245b94a7804576f1d292de157208e60ef711265
sooonia <kopels@g.cofc.edu>
2018-02-19 09:24:18 -0500

    Added nonfunctional join script

---
+++ Join_Data.R
@@ -0,0 +1,13 @@
+library(dplyr)
+load('prp2015.rda')
+prp <- rd.p
+load('fyc2015.rda')
+fyc <- rd.p
+#Remove var to save space
+remove(rd.p)
+
+vars <- c(id, plan.dsn,behaviors, controls, target)
+prp <- droplevels(prp[prp$NAMECHNG == 2,])
+
+meps <- fyc %>% left_join(prp, by=c('DUPERSID'))
+
