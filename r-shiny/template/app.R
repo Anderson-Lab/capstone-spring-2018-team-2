c6a73e7afdc7f98198e71c47017da20a85124408
Alex Giarrocco <grocko95@gmail.com>
2018-02-25 14:56:03 -0500

    closes #29

--- r-shiny/template/app.R
+++ r-shiny/template/app.R
@@ -3,26 +3,21 @@ library(shinydashboard)
 
 ui <- dashboardPage(
   dashboardHeader(title = "capstone"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Investigation", tabName = "investigation", icon = icon("dashboard")),
-      menuItem("Models", tabName = "models", icon = icon("th"))
-    )
+  
+  dashboardSidebar(sidebarMenuOutput("sideMenu")
   ),
   
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "investigation",
+      tabItem(tabName = "investigation", class = "active",
       # Boxes need to be put in a row (or column)
         fluidRow(
           box(plotOutput("plot1", height = 350)),
           
           box(sliderInput("imp", "Select importance level: ", 0, 100, 50, step = 10))
+          
         )
-      ),
-      tabItem(tabName = "models",
-        h2('Models will go here..')
       )
     )
   )
@@ -30,10 +25,19 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   load("data/ranger_imp.rda") # var.importance data generated ranger function in Build_Ranger.R
-  
+  load("data/meta.rda")
+  output$sideMenu <- renderMenu({ sidebarMenu(
+      menuItem("Investigation", 
+               tabName = "investigation", 
+               icon = icon("dashboard"),
+               textInput("var_name", "Check Variable Description", "EVALCOVR"),
+               verbatimTextOutput("var_description"))
+      )
+  });
   output$plot1 <- renderPlot({
     barplot(imp[imp > input$imp], horiz=TRUE, las= 1, xlab = "Importance Level", main = "Variable Importance Plot")
   })
+  output$var_description <- renderText({ meta_named_char[input$var_name] })
 }
 
 shinyApp(ui, server)
--- utils/util_functions.R
+++ utils/util_functions.R
@@ -1,33 +1,36 @@
 meps_scrape = function(){
   library('rvest')
   
-  load("data.rda")
-  
   #Specifying the url for desired website to be scrapped
-  url = "utils/files/meps_codes.html"
-  
+  urls = c("utils/files/fyc_codes.html", "utils/files/prp_codes.html")
   #Reading the HTML code from the website
-  webpage = read_html(url)
+  dfs = list()
   
-  #Using CSS selectors to scrap the rankings section
-  table_data_html = html_nodes(webpage,'.codes')
+  for (i in 1:2){
+    webpage = read_html(urls[i])
+    
+    #Using CSS selectors to scrap the rankings section
+    table_data_html = html_nodes(webpage,'.codes')
+    
+    #Converting the table data to text
+    table_data = html_text(table_data_html)
+    
+    # convert html table to a dataframe
+    codes_df = html_table(table_data_html, header = F, trim = T)[[1]]
+    
+    rownames(codes_df) = codes_df$X1
+    
+    drops <- c("X2","X3")
+    
+    dfs[[i]] = codes_df[ 2:nrow(codes_df), !(names(codes_df) %in% drops)]
+    
+  }
+    
+    full_df = rbind(dfs[[1]], dfs[[2]])
+    # map the variable description in column 'X4' to the codes in rd.p
+    
+    meta_named_char = setNames(full_df$X4, full_df$X1)
   
-  #Converting the table data to text
-  table_data = html_text(table_data_html)
-  
-  # convert html table to a dataframe
-  codes_df = html_table(table_data_html, header = F, trim = T)[[1]]
-  
-  rownames(codes_df) = codes_df$X1
-  
-  codes_df$X1 = NULL
-  
-  # map the variable description in column 'X4' to the codes in rd.p
-
-  meta_named_char = sapply(colnames(rd.p), function(x) codes_df[x, 'X4'] )
-  
-  meta = data.frame(as.list(descriptions))
-  
-  save(meta, file = "meta.rda")
-  
+    save(meta_named_char, file = "meta.rda")
 }
+meps_scrape()
