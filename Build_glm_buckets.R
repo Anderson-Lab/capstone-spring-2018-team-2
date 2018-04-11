1eee5dca316cc6537003bcc9c19602793f765707
sooonia <kopels@g.cofc.edu>
2018-04-11 11:38:38 -0400

    Closes #62 Made ranger models for buckets
    Made bucket ranger models in accordance with Seth's email

--- Build_glm_buckets.R
+++ Build_glm_buckets.R
@@ -1,10 +1,11 @@
 library(caret)
+library(ranger)
 source("Join_Data.R")
 load('mepsBehaviorBucket.rda')
 
 buckets <- droplevels(buckets[buckets$AGE15X >= 40,])
 buckets$w <- buckets$IPDIS15
-buckets[buckets$w<1, 'w']<- 1
+buckets[buckets$w<1, 'w']<- .3
 
 buckets$IPDIS15[buckets$IPDIS15>1] <- 1
 buckets$behave_bucket <- as.factor(buckets$behave_bucket)
@@ -25,7 +26,6 @@ buckets[buckets$OOPPREM < 0, 'OOPPREM'] <- 0
 
 
 for(variable in c(plan.dsn[-c(2,3)], controls[-1])){
-  print(variable)
   buckets[buckets[,variable] < 0, variable] <- 0
 }
 
@@ -43,8 +43,28 @@ x.test <- td[-trainidx,-which(names(td) == target)]
 ds <- downSample(train, train[,target], list = FALSE)
 f <- formula(paste(target, paste(predVars, collapse = '+' ), sep = '~'))
 lin.model <- glm(f,family=binomial(link='logit'),data=train, weights = train$w)
+rf.model <- ranger(formula = f,
+                   data = train,
+                   case.weights = train$w,
+                   num.trees = 2500,
+                   importance = 'impurity',
+                   min.node.size = 150,
+                   probability = TRUE,
+                   classification = TRUE,
+                   sample.fraction = .7
+                   )
 
 lin.model.ds <- glm(f,family=binomial(link='logit'),data=ds, weights = ds$w)
+rf.model.ds <- ranger(formula = f,
+                   data = ds,
+                   case.weights = ds$w,
+                   num.trees = 500,
+                   importance = 'impurity',
+                   min.node.size = 35,
+                   probability = TRUE,
+                   classification = TRUE,
+                   sample.fraction = .7
+)
 
 
 # predict buckets
@@ -53,6 +73,30 @@ target <- 'behave_bucket'
 
 ds <- downSample(train, train[,target], list = FALSE)
 f <- formula(paste(target, paste(predVars, collapse = '+' ), sep = '~'))
-lin.model <- glm(f,family=binomial(link='logit'),data=train, weights = train$w)
+# ONLY RUN GLM CALLS IF 2 BUCKETS
+#lin.model.b <- glm(f,family=binomial(link='logit'),data=train, weights = train$w)
+rf.model.b <- ranger(formula = f,
+                   data = train,
+                   case.weights = train$w,
+                   num.trees = 1500,
+                   importance = 'impurity',
+                   min.node.size = 150,
+                   probability = TRUE,
+                   classification = TRUE,
+                   sample.fraction = .7
+)
 
-lin.model.ds <- glm(f,family=binomial(link='logit'),data=ds, weights = ds$w)
+#lin.model.b.ds <- glm(f,family=binomial(link='logit'),data=ds, weights = ds$w)
+rf.model.b.ds <- ranger(formula = f,
+                     data = ds,
+                     case.weights = ds$w,
+                     num.trees = 500,
+                     importance = 'impurity',
+                     min.node.size = 10,
+                     probability = TRUE,
+                     classification = TRUE,
+                     sample.fraction = .7
+)
+
+
+print("Trained models are: lin.model, lin.model.ds, #lin.model.b, #lin.model.b.ds, rf.model, rf.model.ds,rd.model.b, and rf.model.b.ds")
